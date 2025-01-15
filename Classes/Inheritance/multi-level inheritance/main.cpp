84f93ac5ce47eabdd99fd52b668a90afb619897a
BRIGHTGAINSCOMPANY <162323582+BRIGHTGAINSCOMPANY@users.noreply.github.com>
2025-01-15 12:50:17 +0300

    dealt with multiple inheritance

--- Classes/Inheritance/multi-level inheritance/main.cpp
+++ Classes/Inheritance/multi-level inheritance/main.cpp
@@ -1,6 +1,6 @@
 /*
-A class can also be derived from one class, which is already derived from another class.
 
+A class can also be derived from one class, which is already derived from another class.
 In the following example, MyGrandChild is derived from class MyChild (which is derived from MyClass).
 
 */
